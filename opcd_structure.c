9d7d685f25e7fe8ca8c44f29780efd7a986c09aa
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-01-05 20:55:53 +0000

    [Feature]: structure

---
+++ opcd_structure.c
@@ -0,0 +1,31 @@
+#include "monty.h"
+glova_t glop;
+/**
+ * selector - structure
+ * @op_str: received string
+ * @stack: pointer to stack
+ * @line_number: line number
+ */
+void selector(char *op_str, stack_t **stack, unsigned int line_number)
+{
+	instruction_t sl[] = {
+		{"push", push}, {"pall", pall}, {"pint", pint},
+		{"nop", nop}, {NULL, NULL}
+	};
+
+	int i = 0;
+
+	while (sl[i].opcode != NULL)
+	{
+		if (strcmp(sl[i].opcode, op_str) == 0)
+		{
+			sl[i].f(stack, line_number);
+			return;
+		}
+		i++;
+	}
+	printf("L%d: unknown instruction %s\n", line_number, op_str);
+	free_dlistint(*stack);
+	fclose(glop.fd);
+	exit(EXIT_FAILURE);
+}
