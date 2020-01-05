5859dc900e7129862abdc50062a480e2491ac470
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-01-05 20:54:00 +0000

    [Feature]: monty transcriptor

---
+++ monty.c
@@ -0,0 +1,55 @@
+#include "monty.h"
+glova_t glop;
+
+/**
+ * glop_val - global variable values
+ * @fd: file descriptor
+ */
+void glop_val(FILE *fd)
+{
+	glop.nline = 1;
+	glop.head = NULL;
+	glop.buff = NULL;
+	glop.fd = fd;
+	glop.size = 0;
+}
+/**
+ * main - monty transcriptor
+ * @argc: argument count
+ * @argv: argument vectors
+ * Return: int
+ */
+int main(int argc, char **argv)
+{
+	char *str_tok;
+
+	glop_val(glop.fd);
+
+	if (argc != 2)
+	{
+		dprintf(STDERR_FILENO, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+
+	glop.fd = fopen(argv[1], "r");
+
+	if (glop.fd == NULL)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+
+	while (getline(&(glop.buff), &glop.size, glop.fd) != -1)
+	{
+		if (*glop.buff != '\n')
+		{
+			str_tok = strtok(glop.buff, "\n");
+			_strtok(str_tok, &glop.head, glop.nline);
+		}
+		glop.nline++;
+	}
+	free(glop.buff);
+	fclose(glop.fd);
+	free_dlistint(glop.head);
+	return (0);
+}
