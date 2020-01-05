2856f5f443f75197a3f9257a331bba167efec0b8
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-01-05 20:55:34 +0000

    [Update]: header file

--- monty.h
+++ monty.h
@@ -1,3 +1,15 @@
+#ifndef MONTY_H
+#define MONTY_H
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <ctype.h>
+
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
@@ -9,9 +21,9 @@
  */
 typedef struct stack_s
 {
-        int n;
-        struct stack_s *prev;
-        struct stack_s *next;
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
 } stack_t;
 
 /**
@@ -24,6 +36,52 @@ typedef struct stack_s
  */
 typedef struct instruction_s
 {
-        char *opcode;
-        void (*f)(stack_t **stack, unsigned int line_number);
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
+
+/**
+ * struct glo_va - global structure
+ * @num: value
+ * @nline: line number
+ * @head: pointer to stack's head
+ * @buff: buffer
+ * @fd: file descriptor
+ * @size: size in bytes
+ */
+typedef struct glo_va
+{
+	int num;
+	unsigned int nline;
+	stack_t *head;
+	char *buff;
+	FILE *fd;
+	size_t size;
+} glova_t;
+
+extern glova_t glop;
+
+/* monty.c */
+void glop_val(FILE *fd);
+
+/* opcd_funcs.c */
+void push(stack_t **stack, unsigned int line_number);
+void pall(stack_t **stack, unsigned int line_number);
+void pint(stack_t **stack, unsigned int line_number);
+void pop(stack_t **stack, unsigned int line_number);
+void swap(stack_t **stack, unsigned int line_number);
+
+/* opcd_funcs1.c */
+void nop(stack_t **stack, unsigned int line_number);
+
+/* opcd_structure.c */
+void selector(char *op_str, stack_t **stack, unsigned int line_number);
+
+/* opcd_tok.c */
+int is_num(char *n);
+void _strtok(char *str, stack_t **stack, unsigned int line_number);
+
+/* free_dlistint.c */
+void free_dlistint(stack_t *head);
+
+#endif
