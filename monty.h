491d6d24e20ef927bccb9f5383af6797286a512f
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-01-06 02:06:02 +0000

    [Update]: Adding a prototype

--- monty.h
+++ monty.h
@@ -72,6 +72,7 @@ void pop(stack_t **stack, unsigned int line_number);
 void swap(stack_t **stack, unsigned int line_number);
 
 /* opcd_funcs1.c */
+void add(stack_t **stack, unsigned int line_number);
 void nop(stack_t **stack, unsigned int line_number);
 
 /* opcd_structure.c */
